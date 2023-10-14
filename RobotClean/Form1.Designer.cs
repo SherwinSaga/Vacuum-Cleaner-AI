01da68e65f19cdce5b8f39eace5a2b50467ee618
sherwin <sherwinnikolaisaga@gmail.com>
2023-10-15 01:10:26 +0800

    Add project files.

---
+++ RobotClean/Form1.Designer.cs
@@ -0,0 +1,137 @@
+﻿namespace RobotClean
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.quadrantA = new System.Windows.Forms.Label();
+            this.quadrantB = new System.Windows.Forms.Label();
+            this.quadrantC = new System.Windows.Forms.Label();
+            this.quadrantD = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox1.Location = new System.Drawing.Point(120, 150);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(1000, 1000);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // quadrantA
+            // 
+            this.quadrantA.AutoSize = true;
+            this.quadrantA.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.quadrantA.Location = new System.Drawing.Point(120, 50);
+            this.quadrantA.Name = "quadrantA";
+            this.quadrantA.Size = new System.Drawing.Size(24, 24);
+            this.quadrantA.TabIndex = 3;
+            this.quadrantA.Text = "A";
+            // 
+            // quadrantB
+            // 
+            this.quadrantB.AutoSize = true;
+            this.quadrantB.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.quadrantB.Location = new System.Drawing.Point(396, 62);
+            this.quadrantB.Name = "quadrantB";
+            this.quadrantB.Size = new System.Drawing.Size(66, 24);
+            this.quadrantB.TabIndex = 4;
+            this.quadrantB.Text = "label3";
+            // 
+            // quadrantC
+            // 
+            this.quadrantC.AutoSize = true;
+            this.quadrantC.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.quadrantC.Location = new System.Drawing.Point(263, 62);
+            this.quadrantC.Name = "quadrantC";
+            this.quadrantC.Size = new System.Drawing.Size(66, 24);
+            this.quadrantC.TabIndex = 5;
+            this.quadrantC.Text = "label3";
+            // 
+            // quadrantD
+            // 
+            this.quadrantD.AutoSize = true;
+            this.quadrantD.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.quadrantD.Location = new System.Drawing.Point(161, 93);
+            this.quadrantD.Name = "quadrantD";
+            this.quadrantD.Size = new System.Drawing.Size(66, 24);
+            this.quadrantD.TabIndex = 6;
+            this.quadrantD.Text = "label3";
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(658, 560);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(114, 39);
+            this.button1.TabIndex = 7;
+            this.button1.Text = "START";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(784, 611);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.quadrantD);
+            this.Controls.Add(this.quadrantC);
+            this.Controls.Add(this.quadrantB);
+            this.Controls.Add(this.quadrantA);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.Form1_Paint);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label quadrantA;
+        private System.Windows.Forms.Label quadrantB;
+        private System.Windows.Forms.Label quadrantC;
+        private System.Windows.Forms.Label quadrantD;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ RobotClean/Form1.cs
@@ -0,0 +1,287 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace RobotClean
+{
+    public partial class Form1 : Form
+    {
+        private Image image;
+        private Timer timer;
+        private Robot robot;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            DoubleBuffered = true;
+            image = Properties.Resources.ROBOT;
+            Bitmap bm = new Bitmap(image, 150, 150);
+            pictureBox1.Image = bm;
+
+            robot = new Robot();
+            setLabels();
+            verifyEnvironment();
+            robotCurrentLocation();
+
+        }
+
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            Graphics g = e.Graphics;
+            Pen p = new Pen(Color.Black, 2);
+
+            g.DrawLine(p, 5, 5, 5, 600);
+            g.DrawLine(p, 5, 5, 600, 5);
+            g.DrawLine(p, 5, 600, 600, 600);
+            g.DrawLine(p, 600, 5, 600, 600);
+            g.DrawLine(p, 300, 5, 300, 300);
+            g.DrawLine(p, 5, 300, 300, 300);
+            g.DrawLine(p, 300, 600, 300, 300);
+            g.DrawLine(p, 300, 300, 600, 300);
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            VacuumSetComplete();
+        }
+
+        private void VacuumSetComplete()
+        {
+            
+            if (robot.IsDirty())
+            {
+                int x = robot.GetLocationX();
+                int y = robot.GetLocationY();
+                robot.Clean();
+                verifyEnvironment();
+                Console.WriteLine($"Robot cleaned dirt at ({x}, {y})");
+                robotCurrentLocation();
+
+            }
+            else
+            {
+                Action move = robot.RandomMove();
+                int x = robot.GetLocationX();
+                int y = robot.GetLocationY();
+                Console.WriteLine($"NoOp - Robot moved {move} to ({x}, {y})");
+                robotCurrentLocation();
+            }
+        }
+
+        private void robotCurrentLocation()
+        {
+            int x = robot.GetLocationX();
+            int y = robot.GetLocationY();
+            int tempx = 0;
+            int tempy = 0;
+            if (x == 0 && y == 0)
+            {
+                tempx = 150;
+                tempy = 150;
+            }
+            else if (x == 0 && y == 1)
+            {
+                tempx = 450;
+                tempy = 150;
+            }
+            else if (x == 1 && y == 0)
+            {
+                tempx = 150;
+                tempy = 450;
+            }
+            else if (x == 1 && y == 1)
+            {
+                tempx = 450;
+                tempy = 450;
+            }
+            pictureBox1.Location = new Point(tempx -75, tempy-75);
+        }
+
+        private void setLabels()
+        {
+            quadrantA.Location = new Point(150, 150);
+            quadrantB.Location = new Point(450, 150);
+            quadrantC.Location = new Point(450, 450);
+            quadrantD.Location = new Point(150, 450);
+        }
+
+        private void verifyEnvironment()
+        {
+            int[,] temp = robot.getEnvironment();
+            quadrantA.Text = temp[0, 0] == 1 ? "DIRTY" : "CLEAN";
+            quadrantB.Text = temp[0, 1] == 1 ? "DIRTY" : "CLEAN";
+            quadrantC.Text = temp[1, 1] == 1 ? "DIRTY" : "CLEAN";
+            quadrantD.Text = temp[1, 0] == 1 ? "DIRTY" : "CLEAN";
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            timer = new Timer();
+            timer.Interval = 3000;
+            timer.Tick += timer1_Tick;
+            timer.Start();
+        }
+    }
+
+    public class Robot
+    {
+        private int currX;
+        private int currY;
+        private int[,] environment; // 1 = dirty, 0 = clean
+        private Random random;
+
+        public Robot()
+        {
+            random = new Random();
+            currX = random.Next(2); 
+            currY = random.Next(2); 
+
+            environment = new int[2, 2];
+            for (int i = 0; i < 2; i++)
+            {
+                for (int j = 0; j < 2; j++)
+                {
+                    environment[i, j] = random.Next(2);
+                }
+            }
+        }
+
+      
+
+        public Action RandomMove()
+        {
+            Action[] possibleMoves = { Action.Up, Action.Down, Action.Left, Action.Right };
+            Action randomDirection = possibleMoves[random.Next(4)];
+
+            // ArrayList possible = new ArrayList();
+            // dynamic possible moves
+
+            int choose = random.Next(2);
+            if(GetLocationX() == 0 && GetLocationY() == 0)
+            {
+                
+                if(choose == 0)//right
+                {
+                    currY++;
+                    return Action.Right;
+                }
+                else //down
+                {
+                    currX++;
+                    return Action.Down;
+                }
+
+            }
+
+            if (GetLocationX() == 0 && GetLocationY() == 1)
+            {
+                if (choose == 0)//left
+                {
+                    currY--;
+                    return Action.Left;
+                }
+                else //down
+                {
+                    currX++;
+                    return Action.Down;
+                }
+            }
+            if (GetLocationX() == 1 && GetLocationY() == 0)
+            {
+                if (choose == 0)//right
+                {
+                    currY++;
+                    return Action.Right;
+                }
+                else //up
+                {
+                    currX--;
+                    return Action.Up;
+                }
+            }
+            if (GetLocationX() == 1 && GetLocationY() == 1)
+            {
+                if (choose == 0)//left
+                {
+                    currY--;
+                    return Action.Left;
+                }
+                else //up
+                {
+                    currX--;
+                    return Action.Up;
+                }
+            }
+
+            return Action.NoOp;
+
+            /* inefficient piece of shit
+
+            switch (randomDirection)
+            {
+                case Action.Up:
+                    if (currY > 0) currY--;
+                    else return Action.NoOp;
+                    break;
+                case Action.Down:
+                    if (currY < 1) currY++;
+                    else return Action.NoOp;
+                    break;
+                case Action.Left:
+                    if (currX > 0) currX--;
+                    else return Action.NoOp;
+                    break;
+                case Action.Right:
+                    if (currX < 1) currX++;
+                    else return Action.NoOp;
+                    break;
+                default:
+                    break;
+            }
+            return randomDirection;
+            */
+        }
+
+        public bool IsDirty()
+        {
+            return environment[currX, currY] == 1;
+        }
+
+        public int[,] getEnvironment()
+        {
+            return environment;
+        }
+
+        public void Clean()
+        {
+            environment[currX, currY] = 0;
+        }
+
+        public int GetLocationX()
+        {
+            return currX;
+        }
+
+        public int GetLocationY()
+        {
+            return currY;
+        }
+    }
+
+    public enum Action
+    {
+        Up,
+        Down,
+        Left,
+        Right,
+        NoOp
+    }
+}
